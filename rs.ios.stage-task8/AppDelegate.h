ba61176b380bb483824425375d51e76b92f425e6
zm1ca <zmicier.fied@yandex.ru>
2021-07-15 15:28:55 +0300

    Initial Commit

---
+++ rs.ios.stage-task8/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  rs.ios.stage-task8
+//
+//  Created by Źmicier Fiedčanka on 15.07.21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ rs.ios.stage-task8/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  rs.ios.stage-task8
+//
+//  Created by Źmicier Fiedčanka on 15.07.21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
